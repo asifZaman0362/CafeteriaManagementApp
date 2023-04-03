e09de93f4c118778ee6549685498bf8628accfa8
asifZaman0362 <zero362001@gmail.com>
2023-04-03 20:35:12 +0530

    added attendance functions

--- src/attendance.ts
+++ src/attendance.ts
@@ -1,21 +1,21 @@
-import * as express from 'express';
-import { Router, Request, Response, NextFunction } from 'express';
+import * as express from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 export default router;
 
-function createAttendance(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({status: "Success"});
+function createAttendance(req: Request, res: Response) {
+  return res.status(200).json({ status: "Success" });
 }
 
-function listAttendance(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({});
+function listAttendance(req: Request, res: Response) {
+  return res.status(200).json({});
 }
 
-function updateEntry(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({status: "Success"});
+function updateEntry(req: Request, res: Response) {
+  return res.status(200).json({ status: "Success" });
 }
 
-router.post('/submit', createAttendance);
-router.get('/list', listAttendance);
-router.get('/update', updateEntry);
+router.post("/submit", createAttendance);
+router.get("/list", listAttendance);
+router.get("/update", updateEntry);
--- src/database/attendance.ts
+++ src/database/attendance.ts
@@ -1,24 +1,60 @@
-import mongoose, { model, Document, Schema } from 'mongoose';
+import mongoose, { model, Document, Schema, Types } from "mongoose";
 
 interface IEntry {
-    userid: string,
-    attendance: boolean
+  userid: Types.ObjectId;
+  attendance: boolean;
+}
+
+interface RawEntry {
+  userid: string;
+  attendance: boolean;
 }
 
 interface IAttendance extends Document {
-    date: Date,
-    entries: IEntry[]
+  date: Date;
+  entries: IEntry[];
 }
 
 const EntrySchema = new Schema<IEntry>({
-    userid: { type: String, required: true },
-    attendance: { type: Boolean, required: true }
+  userid: { type: Schema.Types.ObjectId, required: true },
+  attendance: { type: Boolean, required: true },
 });
 
 const AttendanceSchema = new Schema<IAttendance>({
-    date: { type: Date, required: true },
-    entries: { type: [EntrySchema], required: true }
+  date: { type: Date, required: true },
+  entries: { type: [EntrySchema], required: true },
 });
 
-const Attendance = model<IAttendance>('Attendance', AttendanceSchema);
-export default Attendance;
+export const Attendance = model<IAttendance>("Attendance", AttendanceSchema);
+
+export async function addRecord(date: Date, entries: RawEntry[]) {
+  const attendance = new Attendance({
+    date: date,
+    entries: entries.map((entry) => ({
+      userid: new Types.ObjectId(entry.userid),
+      attendance: entry.attendance,
+    })),
+  });
+  return (await attendance.save()) != null;
+}
+
+export async function updateRecord(
+  id: string,
+  date: Date,
+  entries: RawEntry[]
+) {
+  const attendance = await Attendance.findById(id);
+  if (!attendance) return false;
+  attendance.date = date;
+  attendance.entries = entries.map((entry) => ({
+    userid: new Types.ObjectId(entry.userid),
+    attendance: entry.attendance,
+  }));
+  return (await attendance.save()) != null;
+}
+
+export async function removeRecord(id: string) {
+  const attendance = await Attendance.findById(id);
+  if (!attendance) return false;
+  return (await attendance.delete()) != null;
+}
