a7e00061bb38a290dd354e3d78c092abe63fa73a
asifZaman0362 <zero362001@gmail.com>
2023-04-04 11:42:14 +0530

    added menu functions

--- src/menu.ts
+++ src/menu.ts
@@ -1,20 +1,60 @@
 import { NextFunction, Request, Response, Router } from "express";
+import * as database from "./database/menu";
 
 const router = Router();
 export default router;
 
-function addItem(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({status: "Success"});
+async function addItem(req: Request, res: Response) {
+  const { name, price, category_id } = req.body;
+  const result = await database.addItem(name, price, category_id);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
 }
 
-function removeItem(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({status: "Success"});
+async function removeItem(req: Request, res: Response) {
+  const id = req.body.id;
+  const result = await database.removeItem(id);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
 }
 
-function updateItem(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json({status: "Success"});
+async function updateItem(req: Request, res: Response) {
+  const { id, name, price, category_id } = req.body;
+  const result = await database.updateItem(id, name, price, category_id);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
 }
 
-router.post('/addItem', addItem);
-router.post('/removeItem', removeItem);
-router.post('/updateItem', updateItem);
+async function addCategory(req: Request, res: Response) {
+  const name = req.body.name;
+  const result = await database.addCategory(name);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
+}
+
+async function removeCategory(req: Request, res: Response) {
+  const id = req.body.id;
+  const result = await database.removeCategory(id);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
+}
+
+async function updateCategory(req: Request, res: Response) {
+  const { id, name } = req.body;
+  const result = await database.updateCategory(id, name);
+  if (result) {
+    return res.status(200);
+  } else return res.status(500);
+}
+
+router.post("/addItem", addItem);
+router.post("/removeItem", removeItem);
+router.post("/updateItem", updateItem);
+router.post("/addCategory", addCategory);
+router.post("/removeCategory", removeCategory);
+router.post("/updateCategory", updateCategory);
